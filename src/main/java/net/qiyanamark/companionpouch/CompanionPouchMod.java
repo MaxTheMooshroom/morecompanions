13dc99c8461bd036a8003d151eded5c2ec6e3209
Maxine Alexander <25956545+MaxTheMooshroom@users.noreply.github.com>
2025-08-31 16:12:32 -0400

    add curios dependency back in

--- src/main/java/net/qiyanamark/companionpouch/CompanionPouchMod.java
+++ src/main/java/net/qiyanamark/companionpouch/CompanionPouchMod.java
@@ -1,6 +1,5 @@
 package net.qiyanamark.companionpouch;
 
-
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.DistExecutor;
@@ -9,35 +8,28 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.client.event.ScreenEvent;
 
-
 import net.qiyanamark.companionpouch.init.ModItems;
 import net.qiyanamark.companionpouch.init.ModMenus;
 import net.qiyanamark.companionpouch.init.ModNetwork;
 import net.qiyanamark.companionpouch.screen.CompanionPouchScreen;
-
-
+import net.qiyanamark.companionpouch.util.CompanionModifierUtil;
 import net.minecraft.client.gui.screens.MenuScreens;
 
+@Mod(CompanionPouchMod.MOD_ID)
+public class CompanionPouchMod {
+    public static final String MOD_ID = "companionpouch";
 
-@Mod(companionpouch.MOD_ID)
-public class companionpouch {
-    public static final String MOD_ID = "companionpouch"; // <-- set to your real mod id
-
-
-    public companionpouch() {
+    public CompanionPouchMod() {
         IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
         ModItems.REGISTER.register(modBus);
         ModMenus.REGISTER.register(modBus);
         ModNetwork.register();
 
-
         modBus.addListener(this::clientSetup);
 
-
-// Client-only ticking key watcher registered on client dist
+        // Client-only ticking key watcher registered on client dist
         DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientOnly::init);
     }
-
 
     private void clientSetup(final FMLClientSetupEvent event) {
         event.enqueueWork(() -> {
@@ -45,8 +37,7 @@ public class companionpouch {
         });
     }
 
-
     private static class ClientOnly {
-        static void init() { net.qiyanamark.companionpouch.util.CompanionModifierUtil.registerClientKeyWatcher(); }
+        static void init() { CompanionModifierUtil.registerClientKeyWatcher(); }
     }
 }
