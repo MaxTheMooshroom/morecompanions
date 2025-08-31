30ddef342cb697eb5b246c7d86899bea42d0d59d
Maxine Alexander <25956545+MaxTheMooshroom@users.noreply.github.com>
2025-08-31 15:59:09 -0400

    adjust gradle configs

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Forge 1.18.2 Mod Development Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            openjdk17
+            gradle
+            maven
+            git
+            packwiz
+            xvfb-run
+          ];
+
+          JAVA_HOME = pkgs.openjdk17;
+          GRADLE_OPTS = "-Dorg.gradle.jvmargs=-Xmx10G";
+        };
+      });
+}
